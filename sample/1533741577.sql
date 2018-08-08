7f937ce645a5d3933b37a6ba3a1bcbb9c3bd8204
Sergey Drobov <sdrobov@strij.tech>
2018-08-08 18:20:20 +0300

    sample source

---
+++ sample/1533741577.sql
@@ -0,0 +1,1 @@
+INSERT INTO `source` VALUES (1,'Kanobu','https://kanobu.ru/','{\"rssUrl\":\"https://kanobu.ru/rss/news.xml\",\"contentSelector\":\"body > div.main.is-wrap > div.container > div > div.wrapContent > div.news-details-wrapper > article > section\"}','2018-08-08 17:56:34',NULL,'');
